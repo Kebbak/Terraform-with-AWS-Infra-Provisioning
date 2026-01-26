731899483a8c6e0b739575b2a04bdf11bc47068d
kebba <kebba.kanyi@capgemini.com>
2026-01-26 08:32:05 +0100

    add resources

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role" "ec2_role" {
     }]
   })
 }
-
+# ssm is needed for Session Manager instead of ssh to connect to the instance
 resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
